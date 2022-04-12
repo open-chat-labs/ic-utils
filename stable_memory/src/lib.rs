c7aa6b402a41b2cf5e67903055f2b809b68469c6
Hamish Peebles <hamishpeebles@gmail.com>
2022-04-12 12:51:47 +0100

    For buffer size use min of `max_buffer_size` and stable memory size

--- stable_memory/src/lib.rs
+++ stable_memory/src/lib.rs
@@ -1,31 +1,41 @@
-use serde::{Serialize, de::DeserializeOwned};
+use ic_cdk::api::stable::{BufferedStableReader, BufferedStableWriter};
+use serde::{de::DeserializeOwned, Serialize};
+use std::cmp::min;
 use std::error::Error;
 use std::io::{Read, Write};
-use ic_cdk::api::stable::{BufferedStableReader, BufferedStableWriter};
 
-pub fn serialize_to_stable_memory<S: Serialize>(state: S, buffer_size: usize) -> Result<(), impl Error> {
+const WASM_PAGE_SIZE_BYTES: usize = 64 * 1024; // 64KB
+
+pub fn serialize_to_stable_memory<S: Serialize>(
+    state: S,
+    buffer_size: usize,
+) -> Result<(), impl Error> {
     let writer = BufferedStableWriter::new(buffer_size);
     serialize(state, writer)
 }
 
-pub fn deserialize_from_stable_memory<S: DeserializeOwned>(buffer_size: usize) -> Result<S, impl Error> {
+pub fn deserialize_from_stable_memory<S: DeserializeOwned>(
+    max_buffer_size: usize,
+) -> Result<S, impl Error> {
+    let stable_size = ic_cdk::api::stable::stable_size() as usize * WASM_PAGE_SIZE_BYTES;
+    let buffer_size = min(max_buffer_size, stable_size);
     let reader = BufferedStableReader::new(buffer_size);
     deserialize(reader)
 }
 
 fn serialize<T, W>(value: T, writer: W) -> Result<(), impl Error>
-    where
-        T: Serialize,
-        W: Write,
+where
+    T: Serialize,
+    W: Write,
 {
     let mut serializer = rmp_serde::Serializer::new(writer).with_struct_map();
     value.serialize(&mut serializer).map(|_| ())
 }
 
 fn deserialize<T, R>(reader: R) -> Result<T, impl Error>
-    where
-        T: DeserializeOwned,
-        R: Read,
+where
+    T: DeserializeOwned,
+    R: Read,
 {
     let mut deserializer = rmp_serde::Deserializer::new(reader);
     T::deserialize(&mut deserializer)
