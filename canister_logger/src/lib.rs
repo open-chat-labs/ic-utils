94a2ba9eabca288cef87070311f4ce16dd404a6c
Hamish Peebles <hamishpeebles@gmail.com>
2023-01-18 15:52:11 +0000

    Add `canister_logger`

---
+++ canister_logger/src/lib.rs
@@ -0,0 +1,174 @@
+// Inspired by https://github.com/dfinity/ic/blob/master/rs/rust_canisters/canister_log/src/lib.rs
+
+use candid::CandidType;
+use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
+use std::collections::VecDeque;
+use std::io::Write;
+use tracing_subscriber::fmt::format::{FmtSpan, Writer};
+use tracing_subscriber::fmt::time::FormatTime;
+use tracing_subscriber::fmt::Layer;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::Registry;
+
+thread_local! {
+    static LOG: RefCell<LogBuffer> = RefCell::new(LogBuffer::default());
+    static TRACE: RefCell<LogBuffer> = RefCell::new(LogBuffer::default());
+}
+
+pub fn setup(enable_trace: bool) {
+    let log_layer = Layer::default()
+        .with_writer(|| LogWriter::new(false))
+        .json()
+        .with_timer(Timer {})
+        .with_file(true)
+        .with_line_number(true)
+        .with_current_span(false)
+        .with_span_list(false);
+
+    if enable_trace {
+        let trace_layer = Layer::default()
+            .with_writer(|| LogWriter::new(true))
+            .json()
+            .with_timer(Timer {})
+            .with_file(true)
+            .with_line_number(true)
+            .with_current_span(false)
+            .with_span_events(FmtSpan::ENTER);
+
+        Registry::default().with(log_layer).with(trace_layer).init();
+    } else {
+        Registry::default().with(log_layer).init();
+    }
+}
+
+pub fn prepend(logs: Vec<LogEntry>, traces: Vec<LogEntry>) {
+    let existing_logs = LOG.with(|l| l.take().entries);
+    for log in logs.into_iter().chain(existing_logs) {
+        LOG.with(|l| l.borrow_mut().append(log));
+    }
+
+    let existing_traces = TRACE.with(|l| l.take().entries);
+    for trace in traces.into_iter().chain(existing_traces) {
+        TRACE.with(|t| t.borrow_mut().append(trace));
+    }
+}
+
+/// A circular buffer for log messages.
+pub struct LogBuffer {
+    max_capacity: usize,
+    entries: VecDeque<LogEntry>,
+}
+
+impl LogBuffer {
+    /// Creates a new buffer of the specified max capacity.
+    pub fn with_capacity(max_capacity: usize) -> Self {
+        Self {
+            max_capacity,
+            entries: VecDeque::with_capacity(max_capacity),
+        }
+    }
+
+    /// Adds a new entry to the buffer, potentially evicting older entries.
+    pub fn append(&mut self, entry: LogEntry) {
+        while self.entries.len() >= self.max_capacity {
+            self.entries.pop_front();
+        }
+        self.entries.push_back(entry);
+    }
+
+    /// Returns an iterator over entries in the order of their insertion.
+    pub fn iter(&self) -> impl Iterator<Item = &LogEntry> {
+        self.entries.iter()
+    }
+}
+
+impl Default for LogBuffer {
+    fn default() -> Self {
+        LogBuffer {
+            max_capacity: 100,
+            entries: VecDeque::new(),
+        }
+    }
+}
+
+pub fn export_logs() -> Vec<LogEntry> {
+    LOG.with(|l| l.borrow().iter().cloned().collect())
+}
+
+pub fn export_traces() -> Vec<LogEntry> {
+    TRACE.with(|t| t.borrow().iter().cloned().collect())
+}
+
+#[derive(CandidType, Serialize, Deserialize, Clone)]
+pub struct LogEntry {
+    pub timestamp: u64,
+    pub message: String,
+}
+
+struct LogWriter {
+    trace: bool,
+    buffer: Vec<u8>,
+}
+
+impl LogWriter {
+    fn new(trace: bool) -> LogWriter {
+        LogWriter {
+            trace,
+            buffer: Vec::new(),
+        }
+    }
+}
+
+impl Write for LogWriter {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        self.buffer.extend(buf);
+        Ok(buf.len())
+    }
+
+    fn flush(&mut self) -> std::io::Result<()> {
+        let buffer = std::mem::take(&mut self.buffer);
+        let json = String::from_utf8(buffer).unwrap();
+
+        let log_entry = LogEntry {
+            timestamp: private::timestamp_millis(),
+            message: json,
+        };
+
+        let sink = if self.trace { &TRACE } else { &LOG };
+        sink.with(|s| s.borrow_mut().append(log_entry));
+        Ok(())
+    }
+
+    fn write_all(&mut self, buf: &[u8]) -> std::io::Result<()> {
+        self.write(buf).and_then(|_| self.flush())
+    }
+}
+
+struct Timer;
+
+impl FormatTime for Timer {
+    fn format_time(&self, w: &mut Writer) -> std::fmt::Result {
+        let now = private::timestamp_millis();
+
+        w.write_str(&format!("{now}"))
+    }
+}
+
+mod private {
+    use std::time::SystemTime;
+
+    #[cfg(target_arch = "wasm32")]
+    pub fn timestamp_millis() -> u64 {
+        unsafe { ic0::time() as u64 / 1_000_000 }
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    pub fn timestamp_millis() -> u64 {
+        SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u64
+    }
+}
